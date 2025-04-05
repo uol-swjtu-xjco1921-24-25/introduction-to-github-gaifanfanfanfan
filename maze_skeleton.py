908c3b222f30fc013c1ffe46ac5c9e6e68d2a4bf
Zhengze Song <song274869@gmail.com>
2025-04-05 14:20:30 +0800

    继续编写

--- maze_skeleton.py
+++ maze_skeleton.py
@@ -12,8 +12,7 @@ class Maze:
     self.player = (0,0) #Current player position
 
 
-
-  def load_maze(filename:str) -> Maze:
+def load_maze(filename:str) -> Maze:
     """Load maze file and return a Maze object
     Implement notes:
     -Each line represents a row of the maze
@@ -26,3 +25,34 @@ class Maze:
     # Set maze.rows and maze.cols
     # Find and verify the existence and uniqueness of S and E
     return maze
+def validate_maze(maze: Maze)-> bool:
+    """Validate the maze structure
+    Validation rules:
+    -Must be rectangular (all rows of equal length)
+    -Size must be 5 <= rows <= 100 and 5 <= cols <= 100
+    -Must contain one unique S and one unique E
+    """
+    # Example:
+    # if maze.row<5 or maze.rows>100: raise error
+    # Check each row length equals maze.col
+    return True
+def move_player(maze: maze, direction:str) -> bool:
+  """
+  """
+def main():
+  if len(sys.argv) != 2:
+    print('Usage:python maze.py <maze_file>')
+    sys.exit(1)
+
+  try:
+    maze = load_maze(sys.argv[1])
+    if not validate_maze(maze):
+      raise ValueError("Maze validation failed")
+    print("Game start! Use WASD to move, M to view map, Q to quit")
+    while True:
+      cmd = input(">").strip()
+      if not 
+
+
+  except Exception as e:
+    print()
