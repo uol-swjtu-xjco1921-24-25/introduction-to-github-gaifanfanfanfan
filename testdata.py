8e564f6892418f941261d1e8c2e01cf37bb6361e
Zhengze Song <song274869@gmail.com>
2025-04-06 21:26:58 +0800

    更新 testdata.py

--- testdata.py
+++ testdata.py
@@ -43,3 +43,27 @@ def test_valid_maze_normal_win():
 
 def test_invalid_characters():
   """"""
+  maze = os.path.join(TEST_MAZE_DIR,"Z.txt")
+  run_test(maze,expected_errors=["Invalid character 'Z' "])
+
+def test_bound_movement():
+  """"""
+  maze = os.path.join(TEST_MAZE_DIR,"3.txt")
+  inputs = os.path.join(TEST_MAZE_DIR,"3.txt")
+  result = run_test(maze,inputs)
+  assert "out of bounds" in result.stdout
+
+def test_oversized_maze():
+  """"""
+  maze = os.path.join(TEST_MAZE_DIR,"4.txt")
+  run_test(maze,expected_errors=["excceds maximum size "])
+
+if __name__ == "__main__":
+  test_oversized_maze()
+  test_bound_movement()
+  test_invalid_characters()
+
+
+ 
+
+
